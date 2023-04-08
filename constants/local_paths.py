e70f6d31039fdd8ae972e7f982c89dbda2aa0bcc
Iván Bloise <108357900+ibloise@users.noreply.github.com>
2023-04-08 21:24:49 +0200

    Delete local_paths.py

--- constants/local_paths.py
+++
@@ -1,4 +0,0 @@
-class localPaths:
-    DATA_EXCHANGE_PATH = "/run/user/1000/gvfs/smb-share:server=10.35.16.24,share=microsec/.data_exchange/fis_epc"
-    CFX_PATH= "pcr_epc"
-    MICROB_PATH="microb_epc"
