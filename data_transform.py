04452f70eb5a0f15e47cb57ee62061c272c8bf4b
ibloise <ibloise62@gmail.com>
2023-04-27 15:35:06 +0200

    update data_transform. delete transform_data

--- data_transform.py
+++ data_transform.py
@@ -20,3 +20,27 @@ from constants.constants import sqlLoader
 
 #Transformar el sqlLOader.tables para crear las ordenes de pandas
 
+from constants.constants import sqlLoader
+
+
+table = "patients"
+schema = "microb"
+
+
+
+def get_transform_schema(source, table_name, schema = sqlLoader.TABLES):
+    #ToDo: Hay que quitar el hardcode. También hay que poner un par de checks par aver si existe la combinación tabla - source
+    table_conf = schema[table_name]
+    src_table = table_conf[sqlLoader.SOURCES_KEY][source]
+    columns_renamer = {col: confs[source] for col, confs in table_conf["cols"].items()}
+    renamer = {
+    "src_schema": source,
+    "src_table": src_table,
+    "dest_table": table_name,
+    "column_renamer": columns_renamer
+    }
+    
+    return renamer
+
+
+print(get_transform_schema(schema,table))
