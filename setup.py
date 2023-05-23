815d500980572f6f10b5d7c8e5fb0f9fca9246ab
ibloise <ibloise62@gmail.com>
2023-05-23 14:53:46 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name = 'Data exchanger',
+    version = '0.1.0',
+    author = 'Iván Bloise Sánchez',
+    description= 'Exchange data with FIS database',
+    packages=find_packages(),
+    entry_points = {
+    'console_scripts': [
+    'import_cfx=data_load.loader:cfx_main',
+    'import_microb=data_load.loader:microb_main'
+    ]
+    }
+)
